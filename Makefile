21f3a6b7ee16150c2cdeb83dedaeeee9d043ca6b
Adam Olsen <arolsen@gmail.com>
2014-09-30 14:59:26 -0600

    update to use cmsis

--- Makefile
+++ Makefile
@@ -1,25 +1,150 @@
-##
-## This file is part of the libopencm3 project.
-##
-## Copyright (C) 2009 Uwe Hermann <uwe@hermann-uwe.de>
-##
-## This library is free software: you can redistribute it and/or modify
-## it under the terms of the GNU Lesser General Public License as published by
-## the Free Software Foundation, either version 3 of the License, or
-## (at your option) any later version.
-##
-## This library is distributed in the hope that it will be useful,
-## but WITHOUT ANY WARRANTY; without even the implied warranty of
-## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-## GNU Lesser General Public License for more details.
-##
-## You should have received a copy of the GNU Lesser General Public License
-## along with this library.  If not, see <http://www.gnu.org/licenses/>.
-##
+######################################
+# STM32F10x Makefile
+######################################
 
-BINARY = lon
+######################################
+# target
+######################################
+TARGET = Blank
 
-LDSCRIPT = stm32f3.ld
+######################################
+# building variables
+######################################
+# debug build?
+DEBUG = 1
+# build for debug in ram?
+RAMBUILD = 0
+# optimization
+OPT = -O0
+#OPT = -Os
 
-include Makefile.include
+#######################################
+# paths
+#######################################
+# source path
+VPATH = src startup
+# firmware library path
+PERIPHLIBPATH = ./periphlib
+#VPATH += $(PERIPHLIBPATH)/CMSIS/CM3/CoreSupport
+#VPATH += $(PERIPHLIBPATH)/CMSIS/CM3/DeviceSupport/ST/STM32F10x
+#VPATH += $(PERIPHLIBPATH)/STM32F10x_StdPeriph_Driver/src
 
+VPATH += $(PERIPHLIBPATH)/CMSIS/Device/ST/STM32F30x
+VPATH += $(PERIPHLIBPATH)/STM32F30x_StdPeriph_Driver/src
+# Build path
+BUILD_DIR = build
+
+# #####################################
+# source
+# #####################################
+SRCS = \
+  main.c \
+  stm32f30x_it.c \
+  system_stm32f30x.c
+ 
+SRCSASM = \
+  startup_stm32f30x.s 
+
+# #####################################
+# firmware library
+# #####################################
+#   core_cm0.c 
+PERIPHLIB_SOURCES = \
+  stm32f30x_gpio.c \
+  stm32f30x_rcc.c \
+  stm32f30x_tim.c \
+  stm32f30x_dma.c \
+  stm32f30x_rtc.c
+
+#######################################
+# binaries
+#######################################
+CC = arm-none-eabi-gcc
+AS = arm-none-eabi-gcc -x assembler-with-cpp
+CP = arm-none-eabi-objcopy
+AR = arm-none-eabi-ar
+SZ = arm-none-eabi-size
+HEX = $(CP) -O ihex
+BIN = $(CP) -O binary -S
+ 
+#######################################
+# CFLAGS
+#######################################
+# macros for gcc
+DEFS = -DSTM32F30x -DUSE_STDPERIPH_DRIVER
+ifeq ($(RAMBUILD), 1)
+DEFS += -DVECT_TAB_SRAM
+endif
+ifeq ($(DEBUG), 1)
+DEFS += -DDEBUG -D_DEBUG
+endif
+# includes for gcc
+INCLUDES = -Iinc
+# INCLUDES += -I$(PERIPHLIBPATH)/CMSIS/CM3/CoreSupport
+# INCLUDES += -I$(PERIPHLIBPATH)/CMSIS/CM3/DeviceSupport/ST/STM32F10x
+# INCLUDES += -I$(PERIPHLIBPATH)/STM32F10x_StdPeriph_Driver/inc
+INCLUDES += -I$(PERIPHLIBPATH)/CMSIS/Device/ST/STM32F30x/Include
+INCLUDES += -I$(PERIPHLIBPATH)/STM32F30x_StdPeriph_Driver/inc
+INCLUDES += -I$(PERIPHLIBPATH)/CMSIS/Include
+# compile gcc flags
+CFLAGS = -mthumb -mcpu=cortex-m4 $(DEFS) $(INCLUDES) $(OPT) -Wall
+ifeq ($(DEBUG), 1)
+CFLAGS += -g -gdwarf-2 -fno-common -fdata-sections -ffunction-sections
+endif
+# Generate dependency information
+CFLAGS += -MD -MP -MF .dep/$(@F).d
+
+#######################################
+# LDFLAGS
+#######################################
+# link script
+LDSCRIPT = linker/STM32F303VC_FLASH.ld
+# libraries
+LIBS = -Wl,--start-group -lc -lgcc -lnosys -Wl,--end-group
+LIBDIR =
+LDFLAGS = -mthumb -mcpu=cortex-m4 -specs=nano.specs -T$(LDSCRIPT) $(LIBS) -Wl,-Map=$(BUILD_DIR)/$(TARGET).map,--cref -Wl,--gc-sections
+
+
+# default action: build all
+all: $(BUILD_DIR)/$(TARGET).elf $(BUILD_DIR)/$(TARGET).hex
+
+
+#######################################
+# build the application
+#######################################
+# list of firmware library objects
+PERIPHLIB_OBJECTS = $(addprefix $(BUILD_DIR)/,$(notdir $(PERIPHLIB_SOURCES:.c=.o)))
+# list of C program objects
+OBJECTS = $(addprefix $(BUILD_DIR)/,$(notdir $(SRCS:.c=.o)))
+# list of ASM program objects
+OBJECTS += $(addprefix $(BUILD_DIR)/,$(notdir $(SRCSASM:.s=.o)))
+
+$(BUILD_DIR)/%.o: %.c Makefile | $(BUILD_DIR) 
+	$(CC) -c $(CFLAGS) $< -o $@
+
+$(BUILD_DIR)/%.o: %.s Makefile | $(BUILD_DIR)
+	$(AS) -c $(CFLAGS) $< -o $@
+
+$(BUILD_DIR)/$(TARGET).elf: $(OBJECTS) $(PERIPHLIB_OBJECTS) Makefile
+	$(CC) $(OBJECTS) $(PERIPHLIB_OBJECTS) $(LDFLAGS) -o $@
+	$(SZ) $@
+	
+$(BUILD_DIR)/%.hex: $(BUILD_DIR)/%.elf | $(BUILD_DIR)
+	$(HEX) $< $@
+	
+$(BUILD_DIR):
+	mkdir -p $@
+
+
+#######################################
+# delete all user application files
+#######################################
+clean:
+	-rm -fR .dep $(BUILD_DIR)
+  
+#
+# Include the dependency files, should be the last of the makefile
+#
+-include $(shell mkdir .dep 2>/dev/null) $(wildcard .dep/*)
+
+# *** EOF ***
---
+++ src/main.c
@@ -0,0 +1,224 @@
+#include "stm32f30x.h"
+
+typedef struct {
+    int num_leds;
+    uint8_t brightness;
+    volatile uint8_t dma_buffer[2000];
+} ws2812;
+
+ws2812 strip;
+
+const uint8_t period = 59;
+const uint8_t low = 17;
+const uint8_t high = 25;
+const uint8_t reset_len = 25;
+uint8_t set = 0;
+
+
+/*#ifdef USE_FULL_ASSERT
+void assert_failed(uint8_t* file , uint32_t line)
+{
+  while (1);
+}
+#endif*/
+
+void delay(volatile long delay) {
+    delay *= 1000;
+    while(delay--) {}
+}
+
+void ws2812_set_color(uint8_t led, uint8_t r, uint8_t g, uint8_t b) {
+    volatile uint32_t n = (led * 24);
+    volatile uint8_t i;
+
+    if(strip.brightness > 0) {
+        r = (r * strip.brightness) >> 8;
+        g = (g * strip.brightness) >> 8;
+        b = (b * strip.brightness) >> 8;
+    }
+
+    for(i = 8; i-- > 0; n++) {
+        strip.dma_buffer[n] = g & (1 << i) ? high : low;
+    }
+
+    for(i = 8; i-- > 0; n++) {
+        strip.dma_buffer[n] = r & (1 << i) ? high : low;
+    }
+
+    for(i = 8; i-- > 0; n++) {
+        strip.dma_buffer[n] = b & (1 << i) ? high : low;
+    }
+
+}
+
+void ws2812_set_color_single(uint8_t led, uint32_t c) {
+    uint8_t
+        r = (uint8_t)(c >> 16),
+        g = (uint8_t)(c >> 8),
+        b = (uint8_t)(c);
+
+    ws2812_set_color(led, r, g, b);
+}
+
+uint32_t ws2812_color(uint8_t r, uint8_t b, uint8_t g) {
+    return ((uint32_t)r << 16) | ((uint32_t) g << 8) | b;
+}
+
+void ws2812_clear(void) {
+    volatile uint16_t i;
+
+    // initialize all the colors as off
+    for(i = 0; i < strip.num_leds; i++) {
+        ws2812_set_color(i, 0, 0, 0);
+    }
+}
+
+uint32_t wheel(char pos) {
+    if(pos < 85) {
+        return ws2812_color(pos * 3, 255 - pos * 3, 0);
+    }
+    else if(pos < 170) {
+        pos -= 85;
+        return ws2812_color(255 - pos * 3, 0, pos * 3);
+    }
+    else {
+        pos -= 170;
+        return ws2812_color(0, pos * 3, 255 - pos * 3);
+    }
+}
+
+void ws2812_show(void) {
+    volatile uint16_t memaddr;
+    volatile uint16_t buffersize;
+
+    buffersize = (strip.num_leds * 24) + reset_len;
+    memaddr = (strip.num_leds * 24);
+
+    while(memaddr < buffersize) {
+        strip.dma_buffer[memaddr] = 0;
+        memaddr++;
+    }
+
+    DMA_SetCurrDataCounter(DMA1_Channel2, buffersize);
+    DMA_Cmd(DMA1_Channel2, ENABLE);
+    TIM_Cmd(TIM3, ENABLE);
+
+    while(!DMA_GetFlagStatus(DMA1_FLAG_TC2));
+
+    DMA_Cmd(DMA1_Channel2, DISABLE);
+    TIM_Cmd(TIM3, DISABLE);
+    DMA_ClearFlag(DMA1_FLAG_TC2);
+}
+
+void rainbow(uint32_t wait) {
+    volatile uint16_t i, j;
+
+    for(j = 0; j < 256 * 5; j++) {
+        for(i = 0; i < strip.num_leds; i++) {
+            ws2812_set_color_single(
+                i, wheel(((i * 256 / strip.num_leds) + j) & 255));
+        }
+        ws2812_show();
+        delay(wait);
+    }
+}
+
+void setup_gpio(void)
+{
+    RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOA, ENABLE);
+    RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOB, ENABLE);
+
+    // set up pin 5 for the status LED
+    GPIO_InitTypeDef GPIO_InitStructure;
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_5;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
+    GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_2MHz;
+    GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
+    GPIO_Init(GPIOA, &GPIO_InitStructure);
+
+    // set up pin 6 for the timer
+    GPIO_InitTypeDef GPIO_InitStructure2;
+    GPIO_InitStructure2.GPIO_Pin = GPIO_Pin_0;
+    GPIO_InitStructure2.GPIO_Mode = GPIO_Mode_AF;
+    GPIO_InitStructure2.GPIO_OType = GPIO_OType_PP;
+    GPIO_InitStructure2.GPIO_Speed = GPIO_Speed_2MHz;
+    GPIO_InitStructure2.GPIO_PuPd = GPIO_PuPd_NOPULL;
+    GPIO_Init(GPIOB, &GPIO_InitStructure2);
+
+    GPIO_PinAFConfig(GPIOB, GPIO_PinSource0, GPIO_AF_2);
+
+}
+
+void setup_timer(void) {
+    RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM3, ENABLE);
+
+    TIM_TimeBaseInitTypeDef TIM_InitStructure;
+    TIM_InitStructure.TIM_Period = period;
+    TIM_InitStructure.TIM_Prescaler = 0;
+    TIM_InitStructure.TIM_ClockDivision = 0;
+    TIM_InitStructure.TIM_CounterMode = TIM_CounterMode_Up;
+    TIM_TimeBaseInit(TIM3, &TIM_InitStructure);
+
+    // configure the output channel
+    TIM_OCInitTypeDef TIM_OCInitStructure;
+    TIM_OCInitStructure.TIM_OCMode = TIM_OCMode_PWM1;
+    TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable;
+    TIM_OCInitStructure.TIM_Pulse = 0;
+    TIM_OCInitStructure.TIM_OCPolarity = TIM_OCPolarity_High;
+    TIM_OC3Init(TIM3, &TIM_OCInitStructure);
+}
+
+void setup_dma(void) {
+    RCC_AHBPeriphClockCmd(RCC_AHBPeriph_DMA1, ENABLE);
+
+    DMA_InitTypeDef DMA_InitStructure;
+
+    DMA_DeInit(DMA1_Channel2);
+    
+    DMA_InitStructure.DMA_PeripheralBaseAddr = (uint32_t)&TIM3->CCR3;
+/*    DMA_InitStructure.DMA_PeripheralBaseAddr = (uint32_t)TIM3_CCR1_Address;*/
+    DMA_InitStructure.DMA_MemoryBaseAddr = (uint32_t)strip.dma_buffer;
+    DMA_InitStructure.DMA_DIR = DMA_DIR_PeripheralDST;
+    DMA_InitStructure.DMA_PeripheralInc = DMA_PeripheralInc_Disable;
+    DMA_InitStructure.DMA_MemoryInc = DMA_MemoryInc_Enable;
+    DMA_InitStructure.DMA_PeripheralDataSize = DMA_PeripheralDataSize_HalfWord;
+    DMA_InitStructure.DMA_MemoryDataSize = DMA_MemoryDataSize_Byte;
+    DMA_InitStructure.DMA_Mode = DMA_Mode_Normal;
+    DMA_InitStructure.DMA_Priority = DMA_Priority_High;
+    DMA_InitStructure.DMA_M2M = DMA_M2M_Disable;
+    DMA_Init(DMA1_Channel2, &DMA_InitStructure);
+
+    TIM_DMACmd(TIM3, TIM_DMA_CC3, ENABLE);
+}
+
+int main(void)
+{
+  SystemInit();
+  strip.num_leds = 144;
+  strip.brightness = 30;
+
+  setup_gpio();
+  setup_timer();
+  setup_dma();
+  
+  ws2812_set_color(89, 0, 0, 0);
+
+  ws2812_clear();
+
+  while (1)
+  {
+    rainbow(0);
+    if(set) {
+        GPIO_ResetBits(GPIOA, GPIO_Pin_5);
+        set = 0;
+    }
+    else {
+        GPIO_SetBits(GPIOA, GPIO_Pin_5);
+        set = 1;
+    }
+  }
+
+  return 0;
+}
+
